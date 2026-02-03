5e97c2614cc4c36553661da83f47639ac848c038
woodrowg <woodrowg@proton.me>
2026-02-03 13:21:57 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "HFEDesigns";
+export const SITE_DESCRIPTION = "Welcome to HFE Designs, where all facets of Systems Engineering are realized!";
